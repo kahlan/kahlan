ff01f1d727bdbb1476a418a76f9871cc12ec9704
Simon JAILLET <sjaillet@gmail.com>
2013-12-23 14:49:54 +0100

    Update kahlan-config filter for travis.

--- kahlan-config.travis.php
+++ kahlan-config.travis.php
@@ -4,17 +4,15 @@ use kahlan\reporter\coverage\exporter\Coveralls;
 $this->applyFilter('_postProcess', function($chain, $suite, $reporter, $options) {
 	$coverage = $reporter->get('coverage');
 
-	if (!$coverage) {
-		return;
+	if (!$coverage || !isset($options['coverage-coveralls'])) {
+		return $chain->next();
 	}
-	if (isset($options['coverage-coveralls'])) {
-		Coveralls::write([
-			'coverage' => $coverage,
-			'file' => $options['coverage-coveralls'],
-			'service_name' => 'travis-ci',
-			'service_job_id' => getenv('TRAVIS_JOB_ID') ?: null
-		]);
-	}
+	Coveralls::write([
+		'coverage' => $coverage,
+		'file' => $options['coverage-coveralls'],
+		'service_name' => 'travis-ci',
+		'service_job_id' => getenv('TRAVIS_JOB_ID') ?: null
+	]);
 	return $chain->next();
 });
 
