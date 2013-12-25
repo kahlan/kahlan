131a59b690f6ce95f23949285a98ad707e9d40f8
Simon JAILLET <sjaillet@gmail.com>
2013-12-25 19:26:36 +0100

    Refactor the Coverage reporter.

--- src/kahlan/reporter/Coverage.php
+++ src/kahlan/reporter/Coverage.php
@@ -56,39 +56,30 @@ class Coverage extends Terminal {
 	}
 
 	public function end($results) {
-		$metrics = $this->metrics();
 		$this->console("\nCoverage Summary\n----------------\n\n");
+		$this->_renderMetrics($this->metrics(), $this->_verbosity);
+	}
 
+	protected function _renderMetrics($metrics, $verbosity = 0) {
+		$type = $metrics->type();
+		if ($verbosity === 1 && ($type === 'class' || $type === 'function')) {
+			return;
+		}
+		if ($verbosity === 2 && ($type === 'function' || $type === 'method')) {
+			return;
+		}
+		$name = $metrics->name();
 		$stats = $metrics->get();
 		$percent = number_format($stats['percent'], 2);
 		$style = $this->_style($percent);
 		$this->console(str_pad("Lines: {$percent}%", 15), $style);
-		$this->console("({$stats['covered']}/{$stats['eloc']})");
-		$this->console("\n");
-		if (!$this->_verbosity) {
+		$this->console(str_pad("({$stats['covered']}/{$stats['eloc']})", 20));
+		$this->console("{$name}\n");
+		if (!$verbosity) {
 			return;
 		}
-		$this->_render($metrics->childs());
-	}
-
-	protected function _render($metrics) {
-
-		foreach ($metrics as $child) {
-			$type = $child->type();
-			if ($this->_verbosity === 1 && ($type === 'class' || $type === 'function')) {
-				continue;
-			}
-			if ($this->_verbosity === 2 && ($type === 'function' || $type === 'method')) {
-				continue;
-			}
-			$name = $child->name();
-			$stats = $child->get();
-			$percent = number_format($stats['percent'], 2);
-			$style = $this->_style($percent);
-			$this->console(str_pad("Lines: {$percent}%", 15), $style);
-			$this->console(str_pad("({$stats['covered']}/{$stats['eloc']})", 20));
-			$this->console("{$name}\n");
-			$this->_render($child->childs());
+		foreach ($metrics->childs() as $child) {
+			$this->_renderMetrics($child, $verbosity);
 		}
 	}
 
