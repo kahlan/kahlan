1e7e158de27ed1aa6720efe2b4e5d0aefad4adff
Simon JAILLET <sjaillet@gmail.com>
2013-12-30 02:11:54 +0100

    Extract Cli coloration in a specific static class.

--- src/kahlan/reporter/Terminal.php
+++ src/kahlan/reporter/Terminal.php
@@ -8,118 +8,11 @@
 
 namespace kahlan\reporter;
 
+use kahlan\cli\Cli;
 use kahlan\util\String;
 use kahlan\analysis\Debugger;
 
 class Terminal extends Reporter {
-
-	/**
-	 * ANSI/VT100 color/format sequences
-	 *
-	 * @var array
-	 */
-	protected $_vt100 = [
-		'colors' => [
-			'black' => 30,
-			'red' => 31,
-			'green' => 32,
-			'yellow' => 33,
-			'blue' => 34,
-			'magenta' => 35,
-			'cyan' => 36,
-			'white' => 37,
-			'default' => 39
-		],
-		'formats' => [
-			'n' => 0,   //normal
-			'b' => 1,   //bold
-			'd' => 2,   //dim
-			'u' => 4,   //underline
-			'r' => 7,   //reverse
-			'h' => 7,   //hidden
-			's' => 9    //strike
-		]
-	];
-
-	/**
-	 * The default color
-	 *
-	 * @var string
-	 */
-	protected $_vtcolor = 'default';
-
-	/**
-	 * The default background color
-	 *
-	 * @var string
-	 */
-	protected $_vtbackground = 'default';
-
-	/**
-	 * The default style
-	 *
-	 * @var string
-	 */
-	protected $_vtstyle = 'default';
-
-	/**
-	 * Return the ANSI/VT100 number from a color name.
-	 *
-	 * @param  mixed $name A color name string or a ANSI/VT100 number
-	 * @return integer a ANSI/VT100 number
-	 */
-	protected function _vtcolor($name) {
-		return $this->_vt100($name);
-	}
-
-	/**
-	 * Return the ANSI/VT100 number from a backgound color name.
-	 *
-	 * @param  mixed $name A backgound color name string or a ANSI/VT100 number
-	 * @return integer a ANSI/VT100 number
-	 */
-	protected function _vtbackground($name) {
-		if (is_numeric($name)) {
-			return $name + 10;
-		}
-		return $this->_vtcolor($name) + 10;
-	}
-
-	/**
-	 * Return a ANSI/VT100 number from a style name.
-	 *
-	 * @param  mixed $name A style name string or a ANSI/VT100 number
-	 * @return integer a ANSI/VT100 number
-	 */
-	protected function _vtstyle($name) {
-		return isset($this->_vt100['formats'][$name]) ? $this->_vt100['formats'][$name] : 0;
-	}
-
-	/**
-	 * Return a ANSI/VT100 number from a color name.
-	 *
-	 * @param  mixed $name A color name string or a ANSI/VT100 number
-	 * @return integer a ANSI/VT100 number
-	 */
-	protected function _vt100($name) {
-		if (is_numeric($name)) {
-			return $name;
-		}
-		$value = 0;
-		$items = explode(' ', $name);
-
-		if (($name = array_shift($items)) === 'light') {
-			$value += 100;
-			$name = array_shift($items);
-		}
-
-		if (isset($this->_vt100['colors'][$name])) {
-			$value += $this->_vt100['colors'][$name];
-		} else {
-			$value = 39;
-		}
-		return $value;
-	}
 
 	/**
 	 * Print a string to STDOUT.
@@ -136,33 +29,7 @@ class Terminal extends Reporter {
 	 *
 	 */
 	public function console($string, $options = null) {
-		if ($options === null) {
-			echo $string;
-			return;
-		}
-
-		if (is_string($options)) {
-			$options = explode(';', $options);
-			if (strlen($options[0]) === 1) {
-				$options = array_pad($options, 3, 'default');
-				$options = array_combine(['style', 'color', 'background'], $options);
-			} else {
-				$options = ['color' => reset($options)];
-			}
-		}
-
-		$options += [
-			'style' => 'default',
-			'color' => 'default',
-			'background' => 'default'
-		];
-
-		$format = "\e[";
-		$format .= $this->_vtstyle($options['style']) . ';';
-		$format .= $this->_vtcolor($options['color']) . ';';
-		$format .= $this->_vtbackground($options['background']) . 'm';
-
-		echo $format . $string . "\e[0m";
+		echo Cli::color($string, $options);
 	}
 
 	/**
