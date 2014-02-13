01709b679965fcfa7d2b2bb783f1637665432243
Simon JAILLET <sjaillet@gmail.com>
2014-02-14 00:22:02 +0100

    Refactor suite test.

--- spec/kahlan/SuiteSpec.php
+++ spec/kahlan/SuiteSpec.php
@@ -1,70 +1,74 @@
 <?php
 namespace spec;
 
-describe("Suite::before", function() {
+describe("Suite", function() {
 
-	$nb = 0;
+	describe("before", function() {
 
-	before(function() use (&$nb) {
-		$nb++;
+		$nb = 0;
+
+		before(function() use (&$nb) {
+			$nb++;
+		});
+
+		it("passes if `before` has been executed", function() use (&$nb) {
+			expect($nb)->toBe(1);
+		});
+
+		it("passes if `before` has not been executed twice", function() use (&$nb) {
+			expect($nb)->toBe(1);
+		});
+
 	});
 
-	it("passes if `before` has been executed", function() use (&$nb) {
-		expect($nb)->toBe(1);
+	describe("beforeEach", function() {
+
+		$nb = 0;
+
+		beforeEach(function() use (&$nb) {
+			$nb++;
+		});
+
+		it("passes if `beforeEach` has been executed", function() use (&$nb) {
+			expect($nb)->toBe(1);
+		});
+
+		it("passes if `beforeEach` has been executed twice", function() use (&$nb) {
+			expect($nb)->toBe(2);
+		});
+
 	});
 
-	it("passes if `before` has not been executed twice", function() use (&$nb) {
-		expect($nb)->toBe(1);
+	describe("after", function() {
+
+		$nb = 0;
+
+		after(function() use (&$nb) {
+			$nb++;
+		});
+
+		it("passes if `after` has not been executed", function() use (&$nb) {
+			expect($nb)->toBe(0);
+		});
+
 	});
 
-});
+	describe("afterEach", function() {
 
-describe("Suite::beforeEach", function() {
+		$nb = 0;
 
-	$nb = 0;
+		afterEach(function() use (&$nb) {
+			$nb++;
+		});
 
-	beforeEach(function() use (&$nb) {
-		$nb++;
-	});
+		it("passes if `afterEach` has not been executed", function() use (&$nb) {
+			expect($nb)->toBe(0);
+		});
 
-	it("passes if `beforeEach` has been executed", function() use (&$nb) {
-		expect($nb)->toBe(1);
-	});
+		it("passes if `afterEach` has been executed", function() use (&$nb) {
+			expect($nb)->toBe(1);
+		});
 
-	it("passes if `beforeEach` has been executed twice", function() use (&$nb) {
-		expect($nb)->toBe(2);
-	});
-
-});
-
-describe("Suite::after", function() {
-
-	$nb = 0;
-
-	after(function() use (&$nb) {
-		$nb++;
-	});
-
-	it("passes if `after` has not been executed", function() use (&$nb) {
-		expect($nb)->toBe(0);
-	});
-
-});
-
-describe("Suite::afterEach", function() {
-
-	$nb = 0;
-
-	afterEach(function() use (&$nb) {
-		$nb++;
-	});
-
-	it("passes if `afterEach` has not been executed", function() use (&$nb) {
-		expect($nb)->toBe(0);
-	});
-
-	it("passes if `afterEach` has been executed", function() use (&$nb) {
-		expect($nb)->toBe(1);
 	});
 
 });
