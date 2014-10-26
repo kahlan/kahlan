d80854e3e5d1a244f61851296a69a856961f654c
Simon JAILLET <sjaillet@gmail.com>
2014-10-26 12:45:34 +0100

    Test exclusive methods.

--- spec/kahlan/SuiteSpec.php
+++ spec/kahlan/SuiteSpec.php
@@ -1,7 +1,14 @@
 <?php
 namespace spec;
 
+use kahlan\Suite;
+use kahlan\Matcher;
+
 describe("Suite", function() {
+
+    beforeEach(function() {
+        $this->suite = new Suite(['matcher' => new Matcher()]);
+    });
 
     describe("before", function() {
 
@@ -91,6 +98,130 @@ describe("Suite", function() {
 
         it("passes if `afterEach` has been executed once more", function() use (&$nb) {
             expect($nb)->toBe(3);
+        });
+
+    });
+
+    describe("xdescribe", function() {
+
+        it("executes only the exclusive `it`", function() {
+
+            $describe = $this->suite->describe("", function() {
+
+                $this->exectuted = [];
+
+                $this->xdescribe("xdescribe", function() {
+
+                    $this->it("xdescribe it", function() {
+                        $this->exectuted['xit']++;
+                    });
+
+                    $this->it("xdescribe it", function() {
+                        $this->exectuted['xit']++;
+                    });
+
+                });
+
+                $this->describe("describe", function() {
+
+                    $this->it("describe it", function() {
+                        $this->exectuted['it']++;
+                    });
+
+                    $this->it("describe it", function() {
+                        $this->exectuted['it']++;
+                    });
+
+                });
+
+            });
+
+            $this->suite->run();
+
+            expect($describe->exectuted)->toEqual(['xit' => 2]);
+            expect($this->suite->exclusive())->toBe(true);
+            expect($this->suite->status())->toBe(-1);
+
+        });
+
+    });
+
+    describe("xcontext", function() {
+
+        it("executes only the exclusive `it`", function() {
+
+            $describe = $this->suite->describe("", function() {
+
+                $this->exectuted = [];
+
+                $this->xcontext("xcontext", function() {
+
+                    $this->it("xcontext it", function() {
+                        $this->exectuted['xit']++;
+                    });
+
+                    $this->it("xcontext it", function() {
+                        $this->exectuted['xit']++;
+                    });
+
+                });
+
+                $this->context("context", function() {
+
+                    $this->it("context it", function() {
+                        $this->exectuted['it']++;
+                    });
+
+                    $this->it("context it", function() {
+                        $this->exectuted['it']++;
+                    });
+
+                });
+
+            });
+
+            $this->suite->run();
+
+            expect($describe->exectuted)->toEqual(['xit' => 2]);
+            expect($this->suite->exclusive())->toBe(true);
+            expect($this->suite->status())->toBe(-1);
+
+        });
+
+    });
+
+    describe("xit", function() {
+
+        it("executes only the exclusive `it`", function() {
+
+            $describe = $this->suite->describe("", function() {
+
+                $this->exectuted = [];
+
+                $this->it("an it", function() {
+                    $this->exectuted['it']++;
+                });
+
+                $this->xit("an xit", function() {
+                    $this->exectuted['xit']++;
+                });
+
+                $this->it("an it", function() {
+                    $this->exectuted['it']++;
+                });
+
+                $this->xit("an xit", function() {
+                    $this->exectuted['xit']++;
+                });
+
+            });
+
+            $this->suite->run();
+
+            expect($describe->exectuted)->toEqual(['xit' => 2]);
+            expect($this->suite->exclusive())->toBe(true);
+            expect($this->suite->status())->toBe(-1);
+
         });
 
     });
