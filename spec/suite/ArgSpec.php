47de37d988f2f072943abb077b04b0724520cdd0
Simon JAILLET <sjaillet@gmail.com>
2015-01-23 01:57:14 +0100

    Adds specs around the `Arg` class.

---
+++ spec/suite/ArgSpec.php
@@ -0,0 +1,39 @@
+<?php
+namespace kahlan\spec\suite;
+
+use Exception;
+use kahlan\Arg;
+
+describe("Arg", function() {
+
+    describe("::__callStatic()", function() {
+
+        it("creates matcher", function() {
+
+            $arg = Arg::toBe(true);
+            expect($arg->match(true))->toBe(true);
+            expect($arg->match(true))->not->toBe(false);
+
+        });
+
+        it("creates a negative matcher", function() {
+
+            $arg = Arg::notToBe(true);
+            expect($arg->match(true))->not->toBe(true);
+            expect($arg->match(true))->toBe(false);
+
+        });
+
+        it("throws an exception using an undefined matcher name", function() {
+
+            $closure = function() {
+                $arg = Arg::toHelloWorld(true);
+            };
+
+            expect($closure)->toThrow(new Exception('Error, undefined matcher `toHelloWorld`.'));
+
+        });
+
+    });
+
+});
