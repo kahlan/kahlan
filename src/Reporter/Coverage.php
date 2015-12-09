c4b08efa44600ef90be85e4065345d05344f6db7
Simon JAILLET <sjaillet@gmail.com>
2015-12-09 07:49:08 +0100

    Fixes a reference to the old namespace reference.

--- src/Reporter/Coverage.php
+++ src/Reporter/Coverage.php
@@ -11,7 +11,7 @@ class Coverage extends Terminal
      * @var array
      */
     protected static $_classes = [
-        'interceptor' => 'jit\Interceptor'
+        'interceptor' => 'Kahlan\Jit\Interceptor'
     ];
 
     /**
