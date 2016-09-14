b88be244adf3b1211195fdee98e9757c422b725a
Simon JAILLET <sjaillet@gmail.com>
2016-09-14 19:45:17 +0200

    Fix an issue related to a monkey patching wrong implementation.

--- spec/Fixture/Jit/Patcher/Monkey/PlainProcessed.php
+++ spec/Fixture/Jit/Patcher/Monkey/PlainProcessed.php
@@ -1,4 +1,4 @@
-<?php $__KMONKEY__3=\Kahlan\Plugin\Monkey::patched(__NAMESPACE__ , 'time');$__KMONKEY__4__=null;$__KMONKEY__4=\Kahlan\Plugin\Monkey::patched(null, 'name\space\MyClass2', false, $__KMONKEY__4__); ?><?php $__KMONKEY__2__=null;$__KMONKEY__2=\Kahlan\Plugin\Monkey::patched(null, 'name\space\MyClass', false, $__KMONKEY__2__); ?><?php $__KMONKEY__1=\Kahlan\Plugin\Monkey::patched(__NAMESPACE__ , 'mt_rand'); ?><?php $__KMONKEY__0=\Kahlan\Plugin\Monkey::patched(__NAMESPACE__ , 'function_exists'); ?><?php
+<?php $__KMONKEY__0=\Kahlan\Plugin\Monkey::patched(__NAMESPACE__ , 'function_exists');$__KMONKEY__1=\Kahlan\Plugin\Monkey::patched(__NAMESPACE__ , 'mt_rand');$__KMONKEY__2__=null;$__KMONKEY__2=\Kahlan\Plugin\Monkey::patched(null, 'name\space\MyClass', false, $__KMONKEY__2__);$__KMONKEY__3=\Kahlan\Plugin\Monkey::patched(__NAMESPACE__ , 'time');$__KMONKEY__4__=null;$__KMONKEY__4=\Kahlan\Plugin\Monkey::patched(null, 'name\space\MyClass2', false, $__KMONKEY__4__); ?><?php
 use name\space\MyClass as MyAlias;
 use name\space as space;
 
