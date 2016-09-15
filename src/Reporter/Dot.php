0d47b99512bcbcccf29bee1fb1d94126aa35b254
Simon JAILLET <sjaillet@gmail.com>
2016-09-15 15:10:36 +0200

    Update the dot reporter.

--- src/Reporter/Dot.php
+++ src/Reporter/Dot.php
@@ -5,10 +5,38 @@ class Dot extends Terminal
 {
     /**
      * Store the current number of dots.
-     *é
+     *
      * @var integer
      */
     protected $_counter = 0;
+
+    /**
+     * The max number of columns.
+     *
+     * @var integer
+     */
+    protected $_columns = 80;
+
+    /**
+     * The dot area with.
+     *
+     * @var integer
+     */
+    protected $_dotWidth = 0;
+
+    /**
+     * Constructor
+     *
+     * @param array $config The config array. Possible options are:
+     *                      -`'columns'`: the max columns width for dot area.
+     */
+    public function __construct($config = [])
+    {
+        parent::__construct($config);
+        $defaults = ['columns' => 80];
+        $config += $defaults;
+        $this->_columns = $config['columns'];
+    }
 
     /**
      * Callback called before any specs processing.
@@ -18,6 +46,7 @@ class Dot extends Terminal
     public function start($args)
     {
         parent::start($args);
+        $this->_dotWidth = max($this->_columns - 11 - strlen($this->_total) * 2, 10);
         $this->write("\n");
     }
 
@@ -59,7 +88,7 @@ class Dot extends Terminal
     {
         do {
             $this->_write(' ');
-        } while ($this->_counter % 80 !== 0);
+        } while ($this->_counter % $this->_dotWidth !== 0);
 
         $this->write("\n");
 
@@ -83,8 +112,12 @@ class Dot extends Terminal
     {
         $this->write($string, $options);
         $this->_counter++;
-        if ($this->_counter % 80 === 0) {
-            $this->write(' ' . floor(($this->_current * 100) / $this->_total) . "%\n");
+
+        if ($this->_counter % $this->_dotWidth === 0) {
+            $percent = min(floor(($this->_counter * 100) / $this->_total), 100) . '%';
+            $this->write(str_pad($this->_counter, strlen($this->_total) + 1, ' ', STR_PAD_LEFT));
+            $this->write(' / ' . $this->_total);
+            $this->write(' (' . str_pad($percent, 4, ' ', STR_PAD_LEFT) . ")\n");
         }
     }
 }
