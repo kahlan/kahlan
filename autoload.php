3986051f43da50d35228aa9f6485a8d52d1806e9
Simon JAILLET <sjaillet@gmail.com>
2017-10-07 01:10:21 +0200

    Fix CLI autoloading.

--- autoload.php
+++ autoload.php
@@ -12,11 +12,11 @@ return function () {
     }
 
     if ($absolute) {
-        $autoloader = require $absolute;
+        $loader = require $absolute;
     }
 
     if ($pwd === realpath(__DIR__)) {
-        return $autoloader;
+        return $loader;
     }
 
     if (file_exists($composerPath = "{$pwd}/composer.json")) {
@@ -24,33 +24,36 @@ return function () {
         $vendorName = isset($composerJson['vendor-dir']) ? $composerJson['vendor-dir'] : $vendorName;
     }
 
-    if (!file_exists("{$pwd}/{$vendorName}/autoload.php")) {
+    if (file_exists("{$pwd}/{$vendorName}/autoload.php")) {
+        $loader = new ClassLoader();
+
+        $map = require "{$pwd}/{$vendorName}/composer/autoload_namespaces.php";
+        foreach ($map as $namespace => $path) {
+            $loader->set($namespace, $path);
+        }
+
+        $map = require "{$pwd}/{$vendorName}/composer/autoload_psr4.php";
+        foreach ($map as $namespace => $path) {
+            $loader->setPsr4($namespace, $path);
+        }
+
+        $classMap = require "{$pwd}/{$vendorName}/composer/autoload_classmap.php";
+        if ($classMap) {
+            $loader->addClassMap($classMap);
+        }
+
+        $loader->register(true);
+        if (file_exists("{$pwd}/{$vendorName}/composer/autoload_files.php")) {
+            $loader->files(require "{$pwd}/{$vendorName}/composer/autoload_files.php");
+        }
+    }
+
+    if (!$loader) {
         echo "\033[1;31mYou need to set up the project dependencies using the following commands: \033[0m" . PHP_EOL;
         echo 'curl -s http://getcomposer.org/installer | php' . PHP_EOL;
         echo 'php composer.phar install' . PHP_EOL;
         exit(1);
     }
 
-    $loader = new ClassLoader();
-
-    $map = require "{$pwd}/{$vendorName}/composer/autoload_namespaces.php";
-    foreach ($map as $namespace => $path) {
-        $loader->set($namespace, $path);
-    }
-
-    $map = require "{$pwd}/{$vendorName}/composer/autoload_psr4.php";
-    foreach ($map as $namespace => $path) {
-        $loader->setPsr4($namespace, $path);
-    }
-
-    $classMap = require "{$pwd}/{$vendorName}/composer/autoload_classmap.php";
-    if ($classMap) {
-        $loader->addClassMap($classMap);
-    }
-
-    $loader->register(true);
-    if (file_exists("{$pwd}/{$vendorName}/composer/autoload_files.php")) {
-        $loader->files(require "{$pwd}/{$vendorName}/composer/autoload_files.php");
-    }
     return $loader;
 };
