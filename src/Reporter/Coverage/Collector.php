db369d593fc1ba73471046602e2964c0e68b9824
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-12-27 08:25:06 +0700

    [Coverage] use putenv(XDEBUG_MODE=coverage) to support XDebug 3

--- src/Reporter/Coverage/Collector.php
+++ src/Reporter/Coverage/Collector.php
@@ -103,6 +103,8 @@ class Collector
      */
     public function __construct($config = [])
     {
+        putenv('XDEBUG_MODE=coverage');
+
         $defaults = [
             'driver'         => null,
             'path'           => [],
