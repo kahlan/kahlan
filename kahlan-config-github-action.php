39ebff50bde98e3fd7cb2c564c99c3f9dd65f451
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-01-05 16:54:55 +0700

    rename config

--- kahlan-config-github-action.php
+++ kahlan-config-github-action.php
@@ -3,8 +3,6 @@ use Kahlan\Filter\Filters;
 use Kahlan\Reporter\Coverage;
 use Kahlan\Reporter\Coverage\Driver\Xdebug;
 use Kahlan\Reporter\Coverage\Driver\Phpdbg;
-use Kahlan\Reporter\Coverage\Exporter\Coveralls;
-use Kahlan\Reporter\Coverage\Exporter\CodeClimate;
 
 $commandLine = $this->commandLine();
 $commandLine->option('coverage', 'default', 3);
@@ -41,25 +39,4 @@ Filters::apply($this, 'coverage', function($next) {
         'colors'    => !$this->commandLine()->get('no-colors')
     ]);
     $reporters->add('coverage', $coverage);
-});
-
-Filters::apply($this, 'reporting', function($next) {
-    $reporter = $this->reporters()->get('coverage');
-    if (!$reporter) {
-        return;
-    }
-    Coveralls::write([
-        'collector'      => $reporter,
-        'file'           => 'coveralls.json',
-        'service_name'   => 'ci',
-        'service_job_id' => getenv('GITHUB_RUN_NUMBER') ?: null,
-        'repo_token'     => 'cZPzv5qnBk7wyTHPXrIvshctUP5GSTeGt'
-    ]);
-    CodeClimate::write([
-        'collector'  => $reporter,
-        'file'       => 'codeclimate.json',
-        'branch'     => getenv('GITHUB_REF') ?: null,
-        'repo_token' => '422174e17459424c0dc0dfdd720eb17324b283a204b093e85ba21400f1414536'
-    ]);
-    return $next();
 });
